a8cb9781b1fe7af920728436fed350bcff2ae185
Brandon McCarthy <brandon@bmccarthy.net>
2018-01-17 14:39:25 -0800

    Added deafults to tmux and fix multi-monitor issue with polybar

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -3,11 +3,11 @@
 # Terminate already running bar instances
 killall -q polybar
 
-# Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
-# Launch bar1 and bar2
-polybar example &
-
-
-echo "Bars launched..."
+# Find screens and add the bar
+if type "xrandr"; then
+	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do 
+		MONITOR=$m polybar --reload example &
+	done
+else 
+	polybar --reload example &
+fi
