2fab4260e65266211b5dc9a629a8c7add95d0966
Brandon <brandon@bmccarthy.net>
2018-03-18 17:12:37 -0700

    Hide VPN in polybar when off

--- i3/.config/polybar/scripts/vpn.sh
+++ i3/.config/polybar/scripts/vpn.sh
@@ -3,8 +3,6 @@
 vpnconnected=$(nmcli -c no -f NAME,TYPE connection show --active | grep vpn | wc -l)
 vpnname=$(nmcli -c no -f NAME,TYPE connection show --active | grep vpn | sed 's/  vpn//')
 
-if [ "$vpnconnected" -eq "0" ]; then
-	echo "off"
-else
-	echo "$vpnname"
+if [ "$vpnconnected" -eq "1" ]; then
+	echo "$vpnname"
 fi
