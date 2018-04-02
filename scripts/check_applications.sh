989aad8f5165c3199a0435d79e46ab7b4443d328
Brandon <brandon@bmccarthy.net>
2018-04-01 18:23:59 -0700

    Add shell script to check for installed programs

---
+++ scripts/check_applications.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+applications=( i3 betterlockscreen termite vim polybar google-chrome-stable amixer firefox-developer-edition tmux ranger git nitrogen zsh i3lock rofi alsamixer htop xrandr nitrogen clipit xautolock redshift compton weechat nm-applet xfce4-power-manager scrot )
+for i in "${applications[@]}"; do
+	if [[ ! -x $(which "$i" 2>/dev/null) ]]; then
+		echo -e "[\e[31mERROR\e[39m] $i is not in path or executable"
+	else
+		echo -e "[\e[32mFOUND\e[39m] $i is in path and executable"
+	fi
+done
