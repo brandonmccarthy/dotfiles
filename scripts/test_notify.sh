d97b14ee2904eaa3c855ccee4becc36c1af4ea70
Brandon <brandon@bmccarthy.net>
2018-04-03 09:38:22 -0700

    Add file to test dunst

---
+++ scripts/test_notify.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+notify-send -u low "Low test" "This is a low urgency test"
+notify-send -u normal "Normal Test" "This is a normal urgency test"
+notify-send -u critical "Critical Test" "This is a critical alert!"
+
+
